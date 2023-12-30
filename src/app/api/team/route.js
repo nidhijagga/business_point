77e6147950527bc9aceecd93c4aa483483d1fa72
nidhijagga <nidhijagga5239@gmail.com>
2023-12-30 17:21:44 +0530

    Minor Changes

--- src/app/api/team/route.js
+++ src/app/api/team/route.js
@@ -1,14 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
-
 import connect from "@/lib/db";
 import Team from "@/models/TeamModel";
+
 connect();
 
-export async function GET() {
+export async function GET(request) {
   try {
     const jobs = await Team.find();
-    return NextResponse.json(jobs);
+    return new NextResponse(JSON.stringify(jobs), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 200,
+    });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return new NextResponse(JSON.stringify({ error: error.message }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 500,
+    });
   }
 }
